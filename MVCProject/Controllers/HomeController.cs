1922fe122cdd380e71ab337ba834131fd4e91d9e
Tithi Patel <tithi.patel@promptsoftech.in>
2023-09-21 11:29:14 +0530

    first

---
+++ MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using MVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Employee> employeesList = new List<Employee>();
+            employeesList.Add(new Employee { EmpId = 1 , Name = "Zenil" , designation = "CEO"}) ;
+
+            //ViewBag.employeesList = employeesList;
+
+            ViewData["EmployeeList"] = employeesList;
+
+            ViewBag.ViewBag = "Patel";
+            ViewData["Viewdata"] = "Tithi";
+            TempData["Tempdata"] = "Zenil";
+
+            return View();
+            //return View(employeesList);
+        }
+
+        public ActionResult SecondPage()
+        {     
+            ViewBag.t = TempData["Tempdata"];
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.t = TempData["Tempdata"];
+            //throw new Exception();
+            return View();
+        }
+
+        [HandleError(View = "CustomError")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            throw new Exception();
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login Messsage   ";
+
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            //ViewBag.Message = "Login Messsage   ";
+
+            return View();
+        }
+    }
+}
---
+++ MVCProject/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class Employee
+    {
+        public int  EmpId{ get; set; }
+
+        public string Name{ get; set; }
+
+        public string designation{ get; set; }
+    }
+}
